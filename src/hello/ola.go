4582a58c9e9dbfac632fc816777fe66315a82ee8
davipythonweb <galaxys3tab32@gmail.com>
2024-07-25 07:07:14 -0300

    usando operador de inferencia de variavael

--- src/hello/ola.go
+++ src/hello/ola.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var name string = "Eliote"
-	var version float32 = 1.1
-	var age int
+	name := "Eliote"
+	version := 1.1
+	age := 46
 	fmt.Println("Olá, developer", name, ", sua Idade eh:", age)
 	fmt.Println("Este programa está na versão", version)
+
+	fmt.Println("O tipo da variavel name eh:", reflect.TypeOf(name))
+	fmt.Println("O tipo da variavel age eh:", reflect.TypeOf(age))
+	fmt.Println("O tipo da variavel version eh:", reflect.TypeOf(version))
 }
+
+// sem o perador de inferencia de variavel curto
+
+// func main() {
+// 	var name = "Eliote"
+// 	var version = 1.1
+// 	var age = 46
+// 	fmt.Println("Olá, developer", name, ", sua Idade eh:", age)
+// 	fmt.Println("Este programa está na versão", version)
+
+// 	fmt.Println("O tipo da variavel name eh:", reflect.TypeOf(name))
+// 	fmt.Println("O tipo da variavel age eh:", reflect.TypeOf(age))
+// 	fmt.Println("O tipo da variavel version eh:", reflect.TypeOf(version))
+// }
