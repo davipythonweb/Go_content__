d25531feba044ea08886eb74e448baef3a7f04fd
davipythonweb <galaxys3tab32@gmail.com>
2024-07-28 22:39:10 -0300

    update aula 8

--- src/hello/ola.go
+++ src/hello/ola.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
 	name := "Eliote"
 	version := 1.1
-	age := 46
-	fmt.Println("Olá, developer", name, ", sua Idade eh:", age)
+	fmt.Println("Olá, developer", name)
 	fmt.Println("Este programa está na versão", version)
 
-	fmt.Println("O tipo da variavel name eh:", reflect.TypeOf(name))
-	fmt.Println("O tipo da variavel age eh:", reflect.TypeOf(age))
-	fmt.Println("O tipo da variavel version eh:", reflect.TypeOf(version))
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("3- Sair do Programa")
+
+	var comando int
+	// fmt.Scanf("%d", &comando)
+	//ou
+	fmt.Scan(&comando)
+	fmt.Println("O endereço ou ponteiro da variavel comando eh:", &comando)
+
+	fmt.Println("O comando escolhido foi", comando)
 }
+
+// func main() {
+// 	name := "Eliote"
+// 	version := 1.1
+// 	age := 46
+// 	fmt.Println("Olá, developer", name, ", sua Idade eh:", age)
+// 	fmt.Println("Este programa está na versão", version)
+
+// 	fmt.Println("O tipo da variavel name eh:", reflect.TypeOf(name))
+// 	fmt.Println("O tipo da variavel age eh:", reflect.TypeOf(age))
+// 	fmt.Println("O tipo da variavel version eh:", reflect.TypeOf(version))
+// }
 
 // sem o perador de inferencia de variavel curto
 
