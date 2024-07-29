42c5fde14ab94470c61a23909413f4029a85f02e
davipythonweb <galaxys3tab32@gmail.com>
2024-07-28 23:07:03 -0300

    update aula 11

--- src/hello/ola.go
+++ src/hello/ola.go
@@ -15,24 +15,63 @@ func main() {
 	fmt.Println("3- Sair do Programa")
 
 	var comando int
-	// fmt.Scanf("%d", &comando)
-	//ou
-	fmt.Scan(&comando) // o -> & , serve para pegar o endereço ou ponteiro da variavel.
-	fmt.Println("O endereço ou ponteiro da variavel comando eh:", &comando)
+	fmt.Scan(&comando)
 
 	fmt.Println("O comando escolhido foi", comando)
 
-	if comando == 1 {
+	switch comando {
+	case 1:
 		fmt.Println("Monitorando...")
-	} else if comando == 2 {
+	case 2:
 		fmt.Println("Exibindo Logs...")
-
-	} else if comando == 3 {
+	case 3:
 		fmt.Println("Saindo do Programa...")
-	} else {
+	default:
 		fmt.Println("Ops! Comando desconhecido.")
 	}
 }
+
+// func main() {
+// 	name := "Eliote"
+// 	version := 1.1
+// 	fmt.Println("Olá, developer", name)
+// 	fmt.Println("Este programa está na versão", version)
+
+// 	fmt.Println("1- Iniciar Monitoramento")
+// 	fmt.Println("2- Exibir Logs")
+// 	fmt.Println("3- Sair do Programa")
+
+// 	var comando int
+// 	// fmt.Scanf("%d", &comando)
+// 	//ou
+// 	fmt.Scan(&comando) // o -> & , serve para pegar o endereço ou ponteiro da variavel.
+// 	fmt.Println("O endereço ou ponteiro da variavel comando eh:", &comando)
+
+// 	fmt.Println("O comando escolhido foi", comando)
+
+// 	// if comando == 1 {
+// 	// 	fmt.Println("Monitorando...")
+// 	// } else if comando == 2 {
+// 	// 	fmt.Println("Exibindo Logs...")
+
+// 	// } else if comando == 3 {
+// 	// 	fmt.Println("Saindo do Programa...")
+// 	// } else {
+// 	// 	fmt.Println("Ops! Comando desconhecido.")
+// 	// }
+
+// 	// usando switch
+// 	switch comando {
+// 	case 1:
+// 		fmt.Println("Monitorando...")
+// 	case 2:
+// 		fmt.Println("Exibindo Logs...")
+// 	case 3:
+// 		fmt.Println("Saindo do Programa...")
+// 	default:
+// 		fmt.Println("Ops! Comando desconhecido.")
+// 	}
+// }
 
 // func main() {
 // 	name := "Eliote"
